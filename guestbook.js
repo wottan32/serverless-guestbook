51d68a1a785223535f7c3a27c43bba0dd7b88832
Mario Torres <mariotorreslagos@gmail.com>
2021-09-30 12:37:19 -0300

    adding api url
    adding url for ibm course

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://b52b89ee.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
